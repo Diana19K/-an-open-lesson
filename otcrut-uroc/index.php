6472b26c8a629adb3918106baba3c2c69cfbb01a
Natalua9 <148848234+Natalua9@users.noreply.github.com>
2024-03-05 11:58:36 +0500

    Add files via upload

---
+++ otcrut-uroc/index.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <main>
+        
+        <h2 class="name-see">See how Trulia can help</h2>
+        <div class="bloc-see">
+            <div class="red-home">
+                <img src="image\Group 2280.png" alt="">
+                <h3>Rent a home</h3>
+                <p class="text-ipsum">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id risus vulputate tortor, risus ut. Viverra viverra aliquam, semper duis a aliquam.</p>
+            <button class="button-see">Find a rental</button>
+            </div>
+            <div class="see-neigh">
+            <img src="image\Снимок экрана 2024-03-05 095257.png" alt="">
+                <h3>See neighborhoods</h3>
+                <p class="text-ipsum">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id risus vulputate tortor, risus ut. Viverra viverra aliquam, semper duis a aliquam.</p>
+                <button class="button-see">Find a rental</button>
+
+            </div>
+            <div class="buy-home">
+                <img src="image\Group 2281.png" alt="">
+                <h3>Buy a home</h3>
+                <p class="text-ipsum">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id risus vulputate tortor, risus ut. Viverra viverra aliquam, semper duis a aliquam.</p>
+                <button class="button-see">Find a rental</button>
+            
+            </div>
+        </div>
+
+    <div class="seamlessly">
+        <div class="img-seamlessy">
+            <img src="image\Group 2339 (1).png" alt="">
+        </div>
+        <div class="text-seamlessly">
+            <h2 class="name-seamlessly">Seamlessly connecting agents buyers, and sellers  </h2>
+            <p class="text-ipsum"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor at consectetur etiam non amet suscipit dignissim sed. Varius pretium donec dignissim tincidunt sociis. Tellus duis egestas est in venenatis, tincidunt dolor ut eu. Cursus tincidunt urna, quis duis sed massa ornare. Ultricies at quis ante aliquet fringilla.</p>
+            <button class="button-see">Explore more</button>
+        </div>
+    </div>
+
+
+
+<div class="explore-popular">
+    <h2 class="name-seamlessly">Explore the popular residences</h2>
+    <p class="text-lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id risus vulputate tortor, risus ut Viverra viverra aliquam, semper duis a aliquam.</p>
+    <div class="bloc-img-explore">
+        <div class="meadow-wiew">
+        <img src="image\Group 2339 (1).png" alt="">
+            <div class="block-meadow-view">
+                <h3 class="text-meadow">Meadow view</h3>
+                <p>$570.00 | New</p>
+                <p>3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
+            </div>
+        </div>
+        <div class="meadow-wiew">
+        <img src="image\Group 2339 (1).png" alt="">
+        <div class="block-meadow-view">
+                <h3 class="text-meadow">Orchard House</h3>
+                <p>$570.00 | New</p>
+                <p>3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
+            </div>
+        </div>
+        <div class="meadow-wiew">
+        <img src="image\Group 2339 (1).png" alt="">
+        <div class="block-meadow-view">
+                <h3 class="text-meadow">School House</h3>
+                <p>$570.00 | New</p>
+                <p>3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
+            </div>
+        </div>
+    </div>
+</div>
+    </main>
+
+    </div>
+
+
+</body>
+</html>
