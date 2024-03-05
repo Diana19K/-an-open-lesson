06590aeed85f66cd391722f560cd69625ad4b86d
Natalua9 <148848234+Natalua9@users.noreply.github.com>
2024-03-05 12:26:19 +0500

    Add files via upload
    добавление шапки в код

--- otcrut-uroc/index.php
+++ otcrut-uroc/index.php
@@ -8,9 +8,48 @@
     <title>Document</title>
 </head>
 <body>
+<header>
+        <div class="head">
+            <div class="logo">
+                <p>LOGO</p>
+            </div>
+            <div class="href">
+                <a href = "#">Seel House</a>
+                <a href = "#">Rent House</a>
+                <a href = "#">Online Experiences</a>
+            </div>
+            <div class="entrance">
+                <a href = "#">Sign In</a>
+                <button class="button-register">Register</button>
+            </div>
+        </div>
+        <hr>
+        <div class="content">
+            <div class="text">
+                <h1>We help people to getting home & renting with good price</h1>
+                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id risus vulputate tortor, risus ut. Viverra viverra aliquam, semper duis a aliquam. Elit porttitor arcu nisl, faucibus.</p>
+                <div class="txt">
+                    <div class="txt1">
+                        <h2>12k<span>+</span></h2>
+                        <p>Happy Customer</p>
+                    </div>
+                    <div class="txt1">
+                        <h2>14k<span>+</span></h2>
+                        <p>Awards Winning</p>
+                    </div>
+                    <div class="txt1">
+                        <h2>5k<span>+</span></h2>
+                        <p>Premium Product</p>
+                    </div>
+                </div>
+            </div>
+            <div class="img">
+                <img src="images/Group 2339.png" alt="">
+            </div>
+        </div>
+    </header>
 <div class="container">
     <main>
-        
         <h2 class="name-see">See how Trulia can help</h2>
         <div class="bloc-see">
             <div class="red-home">
@@ -24,14 +63,12 @@
                 <h3>See neighborhoods</h3>
                 <p class="text-ipsum">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id risus vulputate tortor, risus ut. Viverra viverra aliquam, semper duis a aliquam.</p>
                 <button class="button-see">Find a rental</button>
-
             </div>
             <div class="buy-home">
                 <img src="image\Group 2281.png" alt="">
                 <h3>Buy a home</h3>
                 <p class="text-ipsum">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id risus vulputate tortor, risus ut. Viverra viverra aliquam, semper duis a aliquam.</p>
                 <button class="button-see">Find a rental</button>
-            
             </div>
         </div>
 
@@ -57,7 +94,7 @@
             <div class="block-meadow-view">
                 <h3 class="text-meadow">Meadow view</h3>
                 <p>$570.00 | New</p>
-                <p>3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
+                <p class="bath-text">3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
             </div>
         </div>
         <div class="meadow-wiew">
@@ -65,7 +102,7 @@
         <div class="block-meadow-view">
                 <h3 class="text-meadow">Orchard House</h3>
                 <p>$570.00 | New</p>
-                <p>3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
+                <p  class="bath-text">3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
             </div>
         </div>
         <div class="meadow-wiew">
@@ -73,7 +110,7 @@
         <div class="block-meadow-view">
                 <h3 class="text-meadow">School House</h3>
                 <p>$570.00 | New</p>
-                <p>3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
+                <p  class="bath-text">3 Bads |  3 Baths  |  2,532 Sq.Ft</p>
             </div>
         </div>
     </div>
